87ba5da187b4fcf56f7ac1f943a5fe7590da82de
loctp2 <loctp2@vng.com.vn>
2023-08-12 01:39:18 +0700

    [Group] Update group model

--- src/types/group.ts
+++ src/types/group.ts
@@ -1,21 +1,5 @@
 export interface Group {
   id: number
-  money: number
-  category: string
-  title: string
-  date: Date
-  currency: string
-  byId: number
-  byName: string
-  participant: Array<number>
-}
-
-export function getMoneyText(group: Group) : string {
-  let money = group.money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + " " + group.currency;
-  return money;
-}
-
-export function getDecriptionText(group: Group) : string {
-  let decr = group.title + " - chi bởi " + group.byName;
-  return decr;
+  groupName: string
+  members: Array<number>
 }
