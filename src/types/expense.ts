a90563575c41787e6e3477f8492d06f107796e09
loctp2 <loctp2@vng.com.vn>
2023-08-12 14:43:27 +0700

    Update money caculate

--- src/types/expense.ts
+++ src/types/expense.ts
@@ -1,30 +1,44 @@
-import { User } from "./user"
+import { toNumber } from "lodash";
+import { User } from "./user";
 
 export interface Expense {
-  id: number,
-  group_id: number,
-  amount: number,
-  user: User,
-  category: string,
-  title: string,
-  date: Date,
-  createdBy: User,
-  participant: Array<User>
+  id: number;
+  group_id: number;
+  amount: number;
+  user: User;
+  category: string;
+  title: string;
+  date: Date;
+  createdBy: User;
+  participant: Array<User>;
 }
 
-export function getMoneyText(expense: Expense) : string {
-  return utilGetMoneyText(expense.amount, "VND")
+export function getMoneyText(expense: Expense): string {
+  return utilGetMoneyText(expense.amount, "VND");
 }
 
-export function utilGetMoneyText(money: number, currency: string) : string {
+export function utilGetMoneyText(money: number, currency: string): string {
   if (money < 0) {
-    money = -money
+    money = -money;
   }
-  let text = money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + " " + currency
-  return text
+  let text = utilGetNumberText(money) + " " + currency;
+  return text;
 }
 
-export function getDecriptionText(expense: Expense) : string {
-  let decr = expense.title + " - chi bởi " + expense.createdBy.name
-  return decr
+export function utilGetNumberText(money: number): string {
+  if (money < 0) {
+    money = -money;
+  }
+  let text = money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return text;
+}
+
+export function utilGetNumberFromText(text: string): number {
+  let temp = text.replace(/,/gm, "");
+  return toNumber(temp);
+}
+
+export function getDecriptionText(expense: Expense): string {
+  let decr = expense.title + " - chi bởi " + expense.createdBy.name;
+  return decr;
 }
